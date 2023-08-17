a507d27145c9805d334120cf150bf37daefd5ad1
agusgarciaruiz <130673674+agusgarciaruiz@users.noreply.github.com>
2023-08-17 07:38:36 -0300

    Add files via upload

---
+++ B_T/inicio.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bolsa de Trabajo - Inicio</title>
+    <link rel="stylesheet" href="CSS/style_inicio.css"> 
+</head>
+<body>
+    <header>
+        <div class="encabezado">
+            <img src="bolsadetrabajo.png" alt="logo de la compañia">
+            <h2 class="name-company">Build The Job</2>
+        </div>
+        <nav>
+            <a href="join.php">Registrarse</a>
+            <a href="./Login_val/login.php" class="nav-link">Iniciar Sesion</a>
+        
+        </nav>
+    </header>
+    <main>
+        <section class="hero">
+            <h1>¡Bienvenido a Build the Job!</h1>
+            <p>Encuentra las mejores oportunidades laborales que se adapten a ti.</p>
+        </section>
+        <section class="about">
+            <h2>Acerca de Nosotros</h2>
+            <p>Somos una plataforma dedicada a conectar a empresas con talento calificado entre estudiantes</p>
+        </section>
+        <section class="services">
+            <h2>Nuestros Servicios</h2>
+            <ul>
+                <li>Explora ofertas de trabajo a tu alcance</li>
+                <li>Crea tu perfil profesional</li>
+                <li>Postula a empleos fácilmente</li>
+            </ul>
+        </section>
+    </main>
+    <footer>
+        <p>&copy; 2023 Build the Job. Todos los derechos reservados.</p>
+    </footer>
+</body>
+</html>
